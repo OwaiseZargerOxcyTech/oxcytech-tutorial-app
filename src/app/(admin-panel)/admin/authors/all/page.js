f5b84042efd5701f78f372e506b622a901e9d427
owaise zarger <owaise.zarger@oxcytech.com>
2024-10-23 11:54:44 +0530

    redesign admin panel sidebar

--- src/app/(admin-panel)/admin/authors/all/page.js
+++ src/app/(admin-panel)/admin/authors/all/page.js
@@ -1,9 +1,11 @@
 "use client";
 import { useSession } from "next-auth/react";
 import AllAuthorsTable from "../_components/AllAuthorsTable";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
   const { data: session, status } = useSession();
+  const router = useRouter();
 
   if (status === "loading") {
     return <div></div>;
@@ -14,6 +16,12 @@ export default function Page() {
   }
   return (
     <div>
+      <button
+        className="p-2 bg-red-500 rounded-md shadow-md mb-2 text-white"
+        onClick={() => router.push("/admin/authors/add")}
+      >
+        Add Authors
+      </button>
       <AllAuthorsTable />
     </div>
   );
--- src/app/(admin-panel)/admin/tutorials/all/page.js
+++ src/app/(admin-panel)/admin/tutorials/all/page.js
@@ -1,9 +1,11 @@
 "use client";
 import { useSession } from "next-auth/react";
 import AllTutorialTable from "../_components/AllTutorialTable";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
   const { data: session, status } = useSession();
+  const router = useRouter();
 
   if (status === "loading") {
     return <div></div>;
@@ -14,6 +16,12 @@ export default function Page() {
   }
   return (
     <div>
+      <button
+        className="p-2 bg-red-500 rounded-md shadow-md mb-2 text-white"
+        onClick={() => router.push("/admin/tutorials/add")}
+      >
+        Add Tutorials
+      </button>
       <AllTutorialTable />
     </div>
   );
